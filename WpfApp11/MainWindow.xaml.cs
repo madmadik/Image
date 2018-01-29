24e0930674865fb05fe371bf23cc5fe98104d544
madik <madik404error@gmail.com>
2018-01-29 20:49:25 +0600

    Добавьте файлы проекта.

---
+++ WpfApp11/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp11
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Find(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog
+            {
+                Title = "Найдите картинку...",
+                InitialDirectory = System.IO.Path.GetFullPath("."),
+            };
+            if (file.ShowDialog() == true)
+            {
+                picture.Source = new BitmapImage(new Uri(file.FileName));
+            }
+            info.Text = file.FileName.Substring(file.FileName.LastIndexOf("\\")).Remove(0, 1);
+        }
+    }
+}
